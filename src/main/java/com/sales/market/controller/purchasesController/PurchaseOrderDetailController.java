cb9111170f22d0b3bea45ae0c8d4891df7bda590
joseandre <contretaa@gmail.com>
2021-09-19 18:46:36 -0400

    add: controler for purcharseorderdetails

--- src/main/java/com/sales/market/controller/purchasesController/PurchaseOrderDetailController.java
+++ src/main/java/com/sales/market/controller/purchasesController/PurchaseOrderDetailController.java
@@ -2,30 +2,34 @@ package com.sales.market.controller.purchasesController;
 
 import com.sales.market.controller.GenericController;
 import com.sales.market.dto.purchasesDto.PurchaseOrderDetailDto;
+import com.sales.market.model.purchases.Provider;
 import com.sales.market.model.purchases.PurchaseOrderDetail;
 import com.sales.market.service.GenericService;
 import com.sales.market.service.purchasesService.ProviderItemService;
 import com.sales.market.service.purchasesService.PurchaseOrderDetailService;
+import com.sales.market.service.purchasesService.PurchaseOrderDetailServiceImpl;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/purchaseorderdetails")
 public class PurchaseOrderDetailController extends GenericController<PurchaseOrderDetail, PurchaseOrderDetailDto> {
-    private PurchaseOrderDetailService service;
+    private PurchaseOrderDetailServiceImpl service;
 
-    public PurchaseOrderDetailController(PurchaseOrderDetailService service) {
+    public PurchaseOrderDetailController(PurchaseOrderDetailServiceImpl service) {
         this.service = service;
     }
 
     @Override
     protected GenericService getService() {
         return service;
+    }
+
+    @GetMapping("/provider-less-total-cost/{idItem}")
+    public Provider getBestProvider(@PathVariable("idItem") Long idProvider){
+        return service.getBestProvider(idProvider);
     }
 
 }
